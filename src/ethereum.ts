40f0a9594dd8fb1766356e007a29bb973a929017
minatx0 <160779971+minatx0@users.noreply.github.com>
2024-05-26 13:24:48 +0200

    Commit ethereum.ts

---
+++ src/ethereum.ts
@@ -0,0 +1,69 @@
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const web3 = new Web3(process.env.ETH_NODE_URL);
+
+export const toWei = (eth: string): string => {
+    return web3.utils.toWei(eth, 'ether');
+};
+
+export const fromWei = (wei: string): string => {
+    return web3.utils.fromWei(wei, 'ether');
+};
+
+export const handleError = (error: Error): void => {
+    console.error("Blockchain error encountered: ", error.message);
+};
+
+export const sendEther = async (fromAddress: string, toAddress: string, amountInEther: string, privateKey: string) => {
+    try {
+        const value = toWei(amountInEther);
+        const nonce = await web3.eth.getTransactionCount(fromaddr, 'latest');
+        const signedTx = await web3.eth.accounts.signTransaction({
+            from: fromAddress,
+            to: toAddress,
+            value: value,
+            gas: 2000000,
+            nonce: nonce
+        }, privateKey);
+
+        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+        return receipt;
+    } catch (error) {
+        handleError(error);
+        throw error;
+    }
+};
+
+export const extractEventData = <T>(eventResult: any, eventName: string): T | null => {
+    if (!eventResult.events[eventName]) return null;
+
+    return eventResult.events[eventName].returnValues as T;
+};
+
+export const interactWithContract = async (contractABI: AbiItem[], contractAddress: string, method: string, params: any[], fromAddress: string, privateKey: string) => {
+    try {
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const data = contract.methods[method](...params).encodeABI();
+
+        const nonce = await web3.eth.getTransactionCount(fromAddress, 'latest');
+        const signedTx = await web3.eth.accounts.signTransaction({
+            to: contractAddress,
+            data: data,
+            gas: 2000000,
+            nonce: nonce
+        }, privateKey);
+
+        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+        return receipt;
+    } catch (error) {
+        handleError(error);
+        throw error;
+    }
+};
